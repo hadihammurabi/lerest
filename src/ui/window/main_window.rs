34b8d49e1ab7ebe6df9ad0a44ee340cd31dcb9b4
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-06-11 21:39:46 +0700

    feat(main_window): display response status code

--- src/ui/window/main_window.rs
+++ src/ui/window/main_window.rs
@@ -25,14 +25,20 @@ pub fn build(app: &gtk::Application) {
     let text_res_data: gtk::TextView = builder.get_object("text_res_data").unwrap();
     let entry_req_uri: gtk::Entry = builder.get_object("entry_req_uri").unwrap();
     let combo_req_method: gtk::ComboBox = builder.get_object("combo_req_method").unwrap();
+    let label_res_status: gtk::Label = builder.get_object("label_res_status").unwrap();
     let req_method: &str = &get_req_method_from_index(combo_req_method.get_active().unwrap() as u8);
 
     let data = ureq::builder()
       .build()
       .request(req_method, &*entry_req_uri.get_text().to_string())
-      .call().unwrap();
+      .call()
+      .unwrap_or(ureq::Response::new(400, "bad request", "").unwrap());
 
-    text_res_data.get_buffer().unwrap().set_text(&data.into_string().unwrap());
+      let status_text = format!("Status: {}", data.status());
+      label_res_status.set_text(&status_text);
+
+      let body = data.into_string().unwrap();
+      text_res_data.get_buffer().unwrap().set_text(&body);
   });
 
   win.show_all();
